1622340ee3737251e2620f9a410cabdd322cf8f8
ulasonat <ulasonat11@gmail.com>
2023-03-04 19:45:01 -0500

    Fetch all URLs after 2020

--- main.py
+++ main.py
@@ -17,11 +17,20 @@ def set_up_session():
     return session
 
 def get_urls():
-    url = 'https://data.commoncrawl.org/crawl-data/CC-MAIN-2020-29/warc.paths.gz'
-    response = requests.get(url)
+    months_to_check = ['2020-16', '2020-24', '2020-29', '2020-34', '2020-40',
+                       '2020-45', '2020-50', '2021-04', '2021-10', '2021-17',
+                       '2021-21', '2021-25', '2021-31', '2021-39', '2021-43',
+                       '2021-49', '2022-05', '2022-21', '2022-27', '2022-33',
+                       '2022-40', '2022-49', '2023-06']
 
-    lines = gzip.decompress(response.content).decode('utf-8').split('\n')
-    urls = [line.strip() for line in lines if line.strip()]
+    urls = []
+
+    for month in months_to_check:
+        url = f'https://data.commoncrawl.org/crawl-data/CC-MAIN-{month}/warc.paths.gz'
+        response = requests.get(url)
+
+        lines = gzip.decompress(response.content).decode('utf-8').split('\n')
+        urls += [line.strip() for line in lines if line.strip()]
 
     return urls
 
@@ -33,6 +42,7 @@ def main():
     regex = re.compile(f"({covid_terms}).*({economic_terms})", re.IGNORECASE)
 
     urls = get_urls()
+
     session = set_up_session()
 
     total_counter = 0
